b95eef94b09a5407cf381095f43d77f008be9b49
Matt <kennedyl@uwec.edu>
2016-09-21 18:20:02 -0500

    Moved arg parsing to utils

---
+++ fsnd_utilities.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+int parse_args(int argc, char **argv);
+int print_help();
--- main.c
+++ main.c
@@ -1,53 +1,15 @@
 #include "fsnd_server.h"
 #include "fsnd_client.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "fsnd_utilities.h"
 
 int main(int argc, char **argv)
 {
-    int opt;
-    int rc;
-
-    while((opt = getopt(argc, argv, "hvpnol")) != -1)
+    int rc = 0;
+    if((rc = parse_args(argc, argv)))
     {
-        switch(opt)
-        {
-            case 'h':
-                if((rc = print_help()))
-                {
-                    printf("%s %d %s print_help() failed. %d. \n",
-                        __FILE__,__LINE__,__func__, rc)
-                }
-                break;
-            case 'v':
-                printf("v\n");
-                break;
-            case 'p':
-                printf("p\n");
-                break;
-            case 'n':
-                printf("n\n");
-                break;
-            case 'o':
-                printf("o\n");
-                break;
-            case 'l':
-                printf("l\n");
-                break;
-        }
+        printf("%s %d %s print_help() failed. Errno: %d. \n",
+               __FILE__,__LINE__,__func__, rc);
     }
-}
-
-int print_help()
-{
-    printf("fsend [OPTIONS] dest_ip file\n");
-    printf("\t-h\t\tPrint this help screen\n");
-    printf("\t-v\t\tVerbose output\n");
-    printf("\t-p port\t\tSet the port to connect on (e.g., 9285\n");
-    printf("\t-n bytes\tNumber of bytes to send, defaults whole file\n");
-    printf("\t-o offset\tOffset into file to start sending\n");
-    printf("\t-l\t\tList (on server side) on port instead of connecting and\n");
-    printf("\t\t\twrite output to file and dest_ip refers to which ip to bind to.\n");
-    printf("\t\t\t(default: localhost)\n");
     return(0);
 }
+
